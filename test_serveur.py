83297584db50a4a21e8148f89542493e19e4d67e
Ihvaan <akyurek.ihvan@gmail.com>
2023-10-23 19:23:10 +0200

    Premier test ok

--- test_serveur.py
+++ test_serveur.py
@@ -1,9 +1,9 @@
 import requests
 
+api_url = "http://127.0.0.1:8000"
+
 
 def test_search_artists():
-    api_url = "http://127.0.0.1:8000"
-
     # Simulate a GET request to the /artists/ endpoint
     response = requests.get(f"{api_url}/artists/?name=Buddy Guy")
 
@@ -11,4 +11,10 @@ def test_search_artists():
     assert response.status_code == 200
 
     # Assert the response content, you can check for specific data or structure
-    assert "Buddy Guy" in print(response.json())
+    assert "Buddy Guy" == response.json()[0]["Name"]
+
+
+def test_search_albums():
+    response = requests.get(f"{api_url}/albums/?artistid=3")
+    assert response.status_code == 200
+    assert "3" == response.json()[0]["ArtistId"]
