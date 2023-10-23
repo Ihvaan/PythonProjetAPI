8ade25cb33eb774e30ed18a434dc658765e89c8a
Ihvaan <akyurek.ihvan@gmail.com>
2023-10-23 17:43:23 +0200

    test_serveur

--- test_serveur.py
+++ test_serveur.py
@@ -2,14 +2,13 @@ import requests
 
 
 def test_search_artists():
-    # Replace 'your_app_url' with the URL where your FastAPI app is running
-    api_url = "http://127.0.0.1:8000/docs#/"
+    api_url = "http://127.0.0.1:8000"
 
     # Simulate a GET request to the /artists/ endpoint
-    response = requests.get(f"{api_url}/artists/?name=ArtistName")
+    response = requests.get(f"{api_url}/artists/?name=Buddy Guy")
 
     # Assert the response status code (200 for success)
     assert response.status_code == 200
 
     # Assert the response content, you can check for specific data or structure
-    assert "ArtistName" in response.json()
+    assert "Buddy Guy" in print(response.json())
